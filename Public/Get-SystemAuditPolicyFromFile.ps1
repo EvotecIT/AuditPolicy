44faa720165f96c8fcebc38947b8984e387da6cf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-05-15 08:53:09 +0200

    Fix for catching errors for get-childitem

--- Public/Get-SystemAuditPolicyFromFile.ps1
+++ Public/Get-SystemAuditPolicyFromFile.ps1
@@ -1,4 +1,17 @@
 ﻿function Get-SystemAuditPolicyFromFile {
+    <#
+    .SYNOPSIS
+    Get local security policy audit policies and group policies that contain audit policies in them (if cache is used).
+
+    .DESCRIPTION
+    Get local security policy audit policies and group policies that contain audit policies in them (if cache is used).
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding()]
     param(
 
@@ -7,7 +20,7 @@
 
     $GPOPath = [io.path]::Combine($Env:SystemRoot, "System32", "GroupPolicy", 'DataStore')
 
-    $GPOWithAudit = Get-ChildItem -LiteralPath $GPOPath -Filter "Audit.csv" -Recurse -Force
+    $GPOWithAudit = Get-ChildItem -LiteralPath $GPOPath -Filter "Audit.csv" -Recurse -Force -ErrorAction SilentlyContinue
     if ($GPOWithAudit) {
         foreach ($File in $GPOWithAudit) {
 
