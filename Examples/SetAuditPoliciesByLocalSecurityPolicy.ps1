8afb1ba1cba9ec39742b10bfbb049c8aa90abbe5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-05-15 19:34:27 +0200

    Added example

---
+++ Examples/SetAuditPoliciesByLocalSecurityPolicy.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\AuditPolicy.psd1 -Force
+
+$T = Get-SystemAuditPolicyFromFile
+$T['LocalSecurityPolicy'] | Format-Table *
+
+Set-SystemAuditPolicy -Policy 'Account Lockout' -Value SuccessAndFailure -UseLocalSecurityPolicy -WhatIf
+Set-SystemAuditPolicy -Policy 'Group Membership' -Value Failure -UseLocalSecurityPolicy
+
+$T = Get-SystemAuditPolicyFromFile
+$T['LocalSecurityPolicy'] | Format-Table *
