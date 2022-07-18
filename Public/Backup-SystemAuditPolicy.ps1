363178a216b7e0231797f31fdaa5dad6e1d1372e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-18 21:47:21 +0200

    Remove duplicate entry

--- Public/Backup-SystemAuditPolicy.ps1
+++ Public/Backup-SystemAuditPolicy.ps1
@@ -100,7 +100,6 @@
             "Detailed Directory Service Replication",
             #Account Logon#
             "Kerberos Service Ticket Operations",
-            "Kerberos Service Ticket Operations",
             "Other Account Logon Events",
             "Kerberos Authentication Service",
             "Credential Validation"
